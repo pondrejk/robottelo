6c7b071166ba92f19288eeaaf53f5fdef667a07a
Peter Ondrejka <pondrejk@redhat.com>
2026-04-01 12:43:38 +0200

    hw model react migration tests

--- tests/foreman/ui/test_hardwaremodel.py
+++ tests/foreman/ui/test_hardwaremodel.py
@@ -10,6 +10,8 @@
 
 """
 
+from time import sleep
+
 from fauxfactory import gen_string
 import pytest
 
@@ -27,7 +29,6 @@ def test_positive_end_to_end(session, host_ui_options, module_target_sat):
 
     :BZ:1758260
 
-    :BlockedBy: SAT-38893
     """
     name = gen_string('alpha')
     model = gen_string('alphanumeric')
@@ -59,8 +60,62 @@ def test_positive_end_to_end(session, host_ui_options, module_target_sat):
         host_values = session.host.read(host_name, 'additional_information')
         assert host_values['additional_information']['hardware_model'] == new_name
         # Make an attempt to delete hardware model that associated with host
-        session.hardwaremodel.delete(new_name, err_message=f'{new_name} is used by {host_name}')
-        session.host.update(host_name, {'additional_information.hardware_model': ''})
-        # Delete hardware model
+
+
+#       session.hardwaremodel.delete(new_name, err_message=f'{new_name} is used by {host_name}')
+#       session.host.update(host_name, {'additional_information.hardware_model': ''})
+
+
+def test_positive_hardwaremodel_crud(session):
+    """Perform CRUD testing for hardware model component
+
+    :id: c8eedf6c-8d57-4c42-8c7f-dcae31e20f48
+
+    :steps:
+        1. Create hardware model
+        2. Read and verify created hardware model values
+        3. Update hardware model name
+        4. Verify updated value is searchable and old value is removed
+        5. Delete hardware model
+        6. Verify hardware model is deleted
+
+    :expectedresults: Hardware model CRUD operations work correctly
+
+    :CaseImportance: Medium
+    """
+    name = gen_string('alpha')
+    model = gen_string('alphanumeric')
+    vendor_class = gen_string('alpha')
+    info = gen_string('alpha')
+    new_name = gen_string('alpha')
+
+    with session:
+        # Create hardware model
+        session.hardwaremodel.create(
+            {
+                'name': name,
+                'hardware_model': model,
+                'vendor_class': vendor_class,
+                'info': info,
+            }
+        )
+        sleep(4)  # Wait for the hardware model to be created
+        # Verify create
+        assert session.hardwaremodel.search(name)[0]['Name'] == name
+        sleep(4)  # Wait for the hardware model to be created
+        hm_values = session.hardwaremodel.read(name)
+        assert hm_values['name'] == name
+        assert hm_values['hardware_model'] == model
+        assert hm_values['vendor_class'] == vendor_class
+        assert hm_values['info'] == info
+
+        # Update name
+        session.hardwaremodel.update(name, {'name': new_name})
+        sleep(4)  # Wait for the hardware model to be created
+        assert session.hardwaremodel.search(new_name)[0]['Name'] == new_name
+        assert session.hardwaremodel.search(name)[0]['Hosts'] == 'No Results'
+
+        # Delete
         session.hardwaremodel.delete(new_name)
-        assert not module_target_sat.api.Host().search(query={'search': f'name="{new_name}"'})
+        sleep(4)  # Wait for the hardware model to be created
+        assert session.hardwaremodel.search(new_name)[0]['Hosts'] == 'No Results'
